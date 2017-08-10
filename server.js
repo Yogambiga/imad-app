7fdb34d0c53b17972ef61a2240a17446552f3b7e
Yogambiga <30728534+Yogambiga@users.noreply.github.com>
2017-08-10 13:52:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var content={
+var articleOne={
     title:'Article-One',
     heading:'Articleone',
     date:'aug 10 2017',
