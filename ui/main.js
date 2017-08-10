1a89de6258f9acaa7f99c011914a2ed77ad62bf5
Yogambiga <30728534+Yogambiga@users.noreply.github.com>
2017-08-10 19:54:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main_text');
 element.innerHTML='My madi';
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft='100px';
+};
