2d2840d5932222915aaff69cc88e9d30539ed04a
Yogambiga <30728534+Yogambiga@users.noreply.github.com>
2017-08-10 20:03:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
    marginLeft=marginLeft+5;
-   img.style.marginLeft=marginLeft+'px';
+   img.style.marginLeft = marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(marginRight,50);
+    var interval=setInterval(moveRight,50);
 };
