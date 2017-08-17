77ecfbdfa44a1d4b6bd33e6ac577c7e026ba84d2
Yogambiga <30728534+Yogambiga@users.noreply.github.com>
2017-08-17 20:26:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,4 @@
-
-
-      
-   
-   var button=document.getElementById('counter');
+ var button=document.getElementById('counter');
 
 button.onclick=function(){
     var request=new XMLHttpRequest();
@@ -19,4 +15,15 @@ button.onclick=function(){
     request.open('GET','http://myoga17.imad.hasura-app.io/counter',true);
     request.send(null);
    
+};
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var names=[yoga,dine,san,mano];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list <= 'li' + names[i] + 'li';
+    }
+    var ul = document.getElementById('namelist');
 };
