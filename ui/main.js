b510c4b9d27c941ca80966f5823abadffe383b61
Yogambiga <30728534+Yogambiga@users.noreply.github.com>
2017-08-17 22:33:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick=function(){
                 var counter=request.responseText;
                 
                var span=document.getElementById('count');
-    span.innerHTML = counter.toString(); 
+    span.innerHTML=counter.toString(); 
             }
         }
     };
@@ -20,11 +20,22 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['yoga','dine','san','mano'];
+     var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
     var list='';
     for(var i=0;i<names.length;i++){
-        list += '<li>' + names[i] + '</li>';
+        list += '<li>' + names[i] + 'li';
     }
     var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    ul.innerHTML=list;
+            }
+        }
+    };
+    request.open('GET','http://myoga17.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
+   
 };
